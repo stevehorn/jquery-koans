9ac1d5e3dd2ed2f7f87b1fef956e465a0ce6d075
Daniele Megna <daniele.megna@xpeppers.com>
2017-09-28 12:49:25 +0200

    Pass false looks little bit strange
    An empty object is a more natural target

--- koans/AboutUtilities.js
+++ koans/AboutUtilities.js
@@ -33,7 +33,7 @@ describe("About Utilities", function() {
     var object1 = { animal: "cardinal", color: "red" };
     var object2 = { firstName: "woody", lastName: "woodpecker" };
     
-    var result = $.extend(false, object1, object2);
+    var result = $.extend({}, object1, object2);
     
     expect(result.lastName).toEqual(__);
   });
